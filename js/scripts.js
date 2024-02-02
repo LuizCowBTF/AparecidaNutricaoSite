7c58cf49eb1897ad7e790de44f912926d0eb2539
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-02 16:15:13 -0300

    error treatament

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,38 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Aparecida Nutricionista";
 
-var paciente = document.querySelector('#primeiro-paciente');
+var pacientes = document.querySelectorAll(".paciente");
 
-var tdPeso = document.querySelector(".info-peso");
-var tdAltura = document.querySelector(".info-altura");
+for(var i = 0; i < pacientes.length; i++) {
+    var paciente = pacientes[i];
 
-var peso = tdPeso.textContent;
-var altura = tdAltura.textContent;
+    var tdPeso = document.querySelector(".info-peso");
+    var peso = tdPeso.textContent;
+    
+    var tdAltura = document.querySelector(".info-altura");
+    var altura = tdAltura.textContent;
+    
+    var pesoValido = true;
+    var alturaValido = true;
 
-var tdIMC = paciente.querySelector(".info-imc");
+    var tdIMC = paciente.querySelector(".info-imc");
 
-
-var pesoValido = true;
-var alturaValido = true;
-
-if(peso <= 30 || peso >= 200) {
-    console.log("Peso inválido!");
-    pesoValido = false;
-    tdIMC.textContent = "Peso inválido";
+    if(peso <= 0 || peso >= 250) {
+        console.log("Peso inválido!");
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+        paciente.classList.add(paciente-invalido);
+    }
+    
+    if(altura <= 0 || altura >= 3.00) {
+        console.log("Altura inválida!");
+        alturaValido = false;
+        tdIMC.textContent = "Altura inválida";
+        paciente.classList.add(paciente-invalido);
+    }
+    
+    if(alturaValido && pesoValido) {
+        var imc = peso / (altura * altura);
+        tdIMC.textContent = imc.toFixed(2);
+    }
 }
-
-if(altura <= 0.50 || altura >= 3.00) {
-    console.log("Altura inválida!");
-    alturaValido = false;
-    tdIMC.textContent = "Altura inválida";
-}
-
-if(alturaValido && pesoValido) {
-    var imc = peso / (altura * altura);
-    tdIMC.textContent = imc;
-}
-
-
