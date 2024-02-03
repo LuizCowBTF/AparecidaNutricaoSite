bd8be1f3ec35cfb00b9bad5771a4a60dc850207b
LuizCowBTF <luizcowbtf@gmail.com>
2024-02-02 23:45:28 -0300

    services api

---
+++ js/buscar-paciente.js
@@ -0,0 +1,25 @@
+var botaoAdicionar = document.querySelector("#buscar-pacientes");
+
+botaoAdicionar.addEventListener("click", function(){
+    console.log("Buscando");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://raw.githubusercontent.com/loresgarcia/Pacientes-API/master/pacientes.json");
+    xhr.addEventListener("load", function(){
+        if(xhr.status == 200){
+            var resposta = xhr.responseText;
+            console.log(resposta);
+            var pacientes = JSON.parse(resposta);
+            
+            pacientes.forEach(function(paciente) {
+                adicionaPacienteTabela(paciente);
+            });
+        }else{
+            console.log(xhr.status);
+            console.log(xhr.responseText);
+            var erroAjax = document.querySelector("#erro-ajax");
+            erroAjax.classList.remove("invisivel");
+        }
+
+    });
+    xhr.send();
+});
--- js/form.js
+++ js/form.js
@@ -3,18 +3,24 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
     var form = document.querySelector("#form-adiciona");
     var paciente = obterPacienteFormulario(form);
-    var pacienteTr = montaTR(paciente);
+    
     var erros = validaPaciente(paciente);
     if(erros.length > 0){
         exibeMensagemErro(erros);
         return;
     }
-    var tabela = document.querySelector("#tabela-pacientes");
-    tabela.appendChild(pacienteTr);
+    
+    adicionaPacienteTabela(paciente);    
     form.reset();
     var mensagensErro = document.querySelector("#mensagens-erro");
     mensagensErro.innerHTML = "";
 });
+
+function adicionaPacienteTabela(paciente) {
+    var pacienteTr = montaTR(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
+}
 
 function obterPacienteFormulario(form) {
     var paciente = {
